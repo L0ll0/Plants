d33f6daf6c2a655bcb3e5a75757d194c659c0613
Lorenzo Vasco <lollo64.lv@gmail.com>
2017-04-02 23:27:48 +0200

    Added three variable as plants

--- Plant_Board/Plant_Board.ino
+++ Plant_Board/Plant_Board.ino
@@ -2,12 +2,18 @@
  * that will be placed near the plants. This board will send the information to the Web-Server board and
  * will actuate the solenoid valve
  */
+#include <VirtualWire.h>
+#define P1 A0
+#define P2 A1
+#define P3 A2
+//These are all the plants that will be included in the project
+
 void setup() {
-  // put your setup code here, to run once:
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
