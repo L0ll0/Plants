66cc90a49ed4bc97069d00f27e5e38d9472f3153
Lorenzo Vasco <lollo64.lv@gmail.com>
2017-04-02 16:38:54 +0200

    Added a new folder with .ino code

---
+++ Web-Server/Web-Server.ino
@@ -0,0 +1,11 @@
+/*This .ino file will contain the code that will be uploaded on the Arduino board that will be connected to the modem. This board will work as a web-server that will write all
+ * necessary informations about plants. It will also have to send a message to the other board whenever you choose to water the plants(by clicking a link on the webpage)
+*/
+void setup() {
+  
+
+}
+
+void loop() {
+  
+}
