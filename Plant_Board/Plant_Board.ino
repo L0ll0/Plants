777f58536fb46db645da1a37270cbd2b2165f1d0
Lorenzo Vasco <lollo64.lv@gmail.com>
2017-04-02 16:46:47 +0200

    Added another folder containing the .ino file for the plant board

---
+++ Plant_Board/Plant_Board.ino
@@ -0,0 +1,13 @@
+/*This .ino file will contain the code that will be uploaded on the board
+ * that will be placed near the plants. This board will send the information to the Web-Server board and
+ * will actuate the solenoid valve
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
