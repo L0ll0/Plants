a69ec30aad6517215ab43a24eb09cbe704cdd513
Lorenzo Vasco <lollo64.lv@gmail.com>
2017-04-07 20:09:05 +0200

    Included Ethernet libraries(then will be deleted unnecessary libraries

--- Web-Server/Web-Server.ino
+++ Web-Server/Web-Server.ino
@@ -1,6 +1,14 @@
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+
 /*This .ino file will contain the code that will be uploaded on the Arduino board that will be connected to the modem. This board will work as a web-server that will write all
  * necessary informations about plants. It will also have to send a message to the other board whenever you choose to water the plants(by clicking a link on the webpage)
 */
+
 void setup() {
   
 
