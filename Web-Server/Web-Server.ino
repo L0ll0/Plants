73a75208548066fb54ac420d53738e806f163821
Lorenzo Vasco <lollo64.lv@gmail.com>
2017-04-08 00:07:44 +0200

    Added some lines of code about the server initialization and the page design

--- Web-Server/Web-Server.ino
+++ Web-Server/Web-Server.ino
@@ -8,12 +8,26 @@
 /*This .ino file will contain the code that will be uploaded on the Arduino board that will be connected to the modem. This board will work as a web-server that will write all
  * necessary informations about plants. It will also have to send a message to the other board whenever you choose to water the plants(by clicking a link on the webpage)
 */
-
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192.168.1.18);
+EthernetServer server(80);
 void setup() {
   
-
+  Ethernet.begin(mac, ip);
+  server.begin();
 }
 
 void loop() {
-  
+  EthernetClient client = server.available();
+  client.println("<!DOCTYPE html>");
+  client.println("<html>");
+  client.println("<head>");
+  client.println("Plants Monitor");
+  client.println("</head>");
+  client.println("<body>");
+  client.println("</body>");
+  client.println("</html>");
+  break;
 }
